8959471a882d46d6b866d5832e5528556e379409
jlafayette <joshlafayette@gmail.com>
2017-11-18 23:53:53 -0800

    Rename main to weapon_dmg

--- bin/graph_weapon_dmg.py
+++ bin/graph_weapon_dmg.py
@@ -6,14 +6,13 @@ import click
 _lib = pathlib.Path(__file__).parents[1].joinpath("lib")
 if _lib not in sys.path:
     sys.path.insert(0, str(_lib))
-from dicemath import main
+from dicemath import weapon_dmg
 
 
 @click.command()
 def graph_weapon_dmg():
-    main.graph_frequency()
-    # main.graph_beat_tgt_frequency()
-    main.graph_value_over_turns(500)
+    weapon_dmg.graph_frequency()
+    weapon_dmg.graph_value_over_turns(500)
 
 
 if __name__ == '__main__':
